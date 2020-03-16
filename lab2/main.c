a48f3df46225aab86cfc70a4605b485107dd6da4
BasicEC <zak.vitaly@yandex.ru>
2020-03-16 18:41:06 +0300

    oh1

--- lab2/main.c
+++ lab2/main.c
@@ -83,7 +83,7 @@ static PartTable def_part_table =
 		sec_in_part: 0xF000 //19000
 	}
 };
-static unsigned int def_log_part_br_abs_start_sector[] = {0x5000, 0xA000};
+static unsigned int def_log_part_br_abs_start_sector[] = {0x5000, 0xA000, 0xF000};
 static const PartTable def_log_part_table[] =
 {
 	{
@@ -92,7 +92,7 @@ static const PartTable def_log_part_table[] =
 			start_head: 0x4,
 			start_sec: 0x1,
 			start_cyl: 0x0,
-			part_type: 0x05, // extended partition type
+			part_type: 0x83, // extended partition type
 			end_sec: 0x29,
 			end_head: 0x5,
 			end_cyl: 0xFF,
